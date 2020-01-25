1eca2355a64dd0bcc150d6d8eb545162df877fc2
cameroo52 <wifitofu@gmail.com>
2020-01-24 19:31:19 -0800

    Completed all necessary requirements for style 1, still need to finish style 2 and errors readme

--- Board.h
+++ Board.h
@@ -9,142 +9,175 @@ using namespace std;
 
 class Board{
 private:
-int length;
-int width;
-int blackScore;
-int whiteScore;
+    int length;
+    int width;
+    int blackScore;
+    int whiteScore;
 
-vector<vector<int>> grid;
+    vector<vector<int>> grid;
 
 public:
-Board(int length, int width){
-    this->length = length;
-    this->width = width;
-    blackScore = 0;
-    whiteScore = 0;
-    buildGrid();
-}
 
-void buildGrid(){
-    vector<int> tempPieces;
-    for (int i = 0; i < length; ++i){
-        grid.push_back(tempPieces);
-        for (int j = 0; j < width; ++j){
-            grid[i].push_back(0);
-        }
+    // Constructor
+    // Requires: non-negative input
+    // Modifies: length, width, blackScore, whiteScore
+    // Effects: Initializes fields, runs buildGrid function
+    Board(int length, int width){
+        this->length = length;
+        this->width = width;
+        blackScore = 0;
+        whiteScore = 0;
+        buildGrid();
     }
 
-    for (int i = 0; i < length/2-1; ++i){
-        for (int j = 0; j < width; ++j){
-            if (((i+1) % 2) != ((j+1) % 2)){
-                grid[i][j] = -1;
+    // Requires: nothing
+    // Modifies: grid
+    // Effects: populates grid vector with vectors of integers to represent the checker pieces and tiles
+    void buildGrid(){
+        //Populates grid with vectors containing integer values of 0 to represent an empty tile
+        vector<int> tempPieces;
+        for (int i = 0; i < length; ++i){
+            grid.push_back(tempPieces);
+            for (int j = 0; j < width; ++j){
+                grid[i].push_back(0);
+            }
+        }
+
+        //Change some default integers in grid to 1 or -1 to represent black and white pieces
+        for (int i = 0; i < length/2-1; ++i){
+            for (int j = 0; j < width; ++j){
+                if (((i+1) % 2) != ((j+1) % 2)){
+                    grid[i][j] = -1;
+                }
+            }
+        }
+        for (int i = length/2+1; i < length; ++i){
+            for (int j = 0; j < width; ++j){
+                if (((i+1) % 2) != ((j+1) % 2)){
+                    grid[i][j] = 1;
+                }
             }
         }
     }
-    for (int i = length/2+1; i < length; ++i){
-        for (int j = 0; j < width; ++j){
-            if (((i+1) % 2) != ((j+1) % 2)){
-                grid[i][j] = 1;
-            }
-        }
-    }
-}
 
-void printBoard(){
-    cout << "BLACK TEAM SCORE: " << blackScore << endl;
-    cout << "WHITE TEAM SCORE: " << whiteScore << endl;
-    for (int i = 0; i < length; ++i){
-        for (int j = 0; j < width; ++j){
-            
-            if (grid[i][j] != 0){
-                if (grid[i][j] == -1){
-                    cout << "W ";
-                }
-                else if (grid[i][j] == 1){
-                    cout << "B ";
-                }
-            }else{
-                if (((i+1) % 2) == ((j+1) % 2)){
-                    cout << "- ";
+    // Requires: nothing
+    // Modifies: cout
+    // Effects: translates the integer values contained in the grid vector to strings for the board display
+    void printBoard(){
+        //Display team scores
+        cout << "\nBLACK TEAM SCORE: " << blackScore << endl;
+        cout << "WHITE TEAM SCORE: " << whiteScore << endl;
+
+        //Translate integers in grid to strings to clarify board contents when printed to console
+        for (int i = 0; i < length; ++i){
+            for (int j = 0; j < width; ++j){
+                
+                if (grid[i][j] != 0){
+                    if (grid[i][j] == -1){
+                        cout << "W ";
+                    }
+                    else if (grid[i][j] == 1){
+                        cout << "B ";
+                    }
                 }else{
-                    cout << "- ";
+                    if (((i+1) % 2) == ((j+1) % 2)){
+                        cout << "- ";
+                    }else{
+                        cout << "- ";
+                    }
+                }
+                
+            }
+            cout << endl;
+        } 
+    }
+
+    // Requires: non-negative input between 1 and 8, string with a value of either "white's" or "black's"
+    // Modifies: blackScore, whiteScore, grid, cout
+    // Effects: rearranges and replaces integers contained within the grid to represent piece movement, and skipping
+    bool movePiece(int row, int column, int moveRow, int moveColumn, string player){
+        //Initialize boolean variables based on nearby pieces/tiles
+        bool hasMoved = false;
+        bool isBlackPiece = checkForPiece(moveRow, moveColumn, "black's");
+        bool isWhitePiece = checkForPiece(moveRow, moveColumn, "white's");
+        bool isEmptyTile = !checkForPiece(moveRow, moveColumn, "white's") && !checkForPiece(moveRow, moveColumn, "black's");
+        bool isValid;
+        bool isValidSkip;
+
+        //Determine if proposed movement position is valid, and increment score if a skip is possible
+        if (player == "black's"){
+            isValid = ((row - moveRow == 1) && (abs(column - moveColumn) == 1));
+            isValidSkip = (isValid && isWhitePiece);
+            blackScore += 1 * isValidSkip;
+        }
+        if (player == "white's"){
+            isValid = ((moveRow - row == 1) && (abs(column - moveColumn) == 1));
+            isValidSkip = (isValid && isBlackPiece);
+            whiteScore += 1 * isValidSkip;
+        }
+
+        //Move the piece if position is valid, default to a skip if one is possible
+        int input;
+        vector<vector<int>> oldGrid = grid;
+        if ((isEmptyTile && isValid) || isValidSkip){
+            if (isValidSkip){
+                cout << player + " piece is skipping!" << endl;
+                oldGrid[moveRow - (row - moveRow)][moveColumn - (column - moveColumn)] = oldGrid[row][column];
+                oldGrid[moveRow][moveColumn] = 0;
+                oldGrid[row][column] = 0;
+                grid = oldGrid;
+                hasMoved = true;
+            }else{
+                oldGrid[moveRow][moveColumn] = oldGrid[row][column];
+                oldGrid[row][column] = 0;
+                grid = oldGrid;
+                hasMoved = true;
+            }
+        }
+        else if (moveRow == -1 || moveColumn == -1){
+            hasMoved = true;
+        }
+        return hasMoved;
+
+    }
+
+    // Requires: non-negative input between 1 and 8, string with a value of either "white's" or "black's"
+    // Modifies: cout
+    // Effects: checks if a piece on the board is present at the input position, and determines the piece's corresponding team
+    bool checkForPiece(int pieceRow, int pieceColumn, string player){
+        //Determine team of piece at given position, or if tile is empty
+        bool found = false;
+        if (pieceRow > -1 && pieceColumn > -1 && pieceRow <= length && pieceColumn <= width){
+            if (player == "white's"){
+                if (grid[pieceRow][pieceColumn] == -1){
+                    found = true;
                 }
             }
-            
-        }
-        cout << endl;
-    } 
-}
-
-bool movePiece(int row, int column, int moveRow, int moveColumn, string player){
-    bool hasMoved = false;
-    bool isBlackPiece = checkForPiece(moveRow, moveColumn, "black's");
-    bool isWhitePiece = checkForPiece(moveRow, moveColumn, "white's");
-    bool isEmptyTile = !checkForPiece(moveRow, moveColumn, "white's") && !checkForPiece(moveRow, moveColumn, "black's");
-    bool isValid;
-    bool isValidSkip;
-
-    if (player == "black's"){
-        isValid = ((row - moveRow == 1) && (abs(column - moveColumn) == 1));
-        isValidSkip = (isValid && isWhitePiece);
-        blackScore += 1 * isValidSkip;
-    }
-    if (player == "white's"){
-        isValid = ((moveRow - row == 1) && (abs(column - moveColumn) == 1));
-        isValidSkip = (isValid && isBlackPiece);
-        whiteScore += 1 * isValidSkip;
-    }
-
-    int input;
-    vector<vector<int>> oldGrid = grid;
-    if ((isEmptyTile && isValid) || isValidSkip){
-        if (isValidSkip){
-            cout << "Skipping!" << endl;
-            oldGrid[moveRow - (row - moveRow)][moveColumn - (column - moveColumn)] = oldGrid[row][column];
-            oldGrid[moveRow][moveColumn] = 0;
-            oldGrid[row][column] = 0;
-            grid = oldGrid;
-            hasMoved = true;
-        }else{
-            oldGrid[moveRow][moveColumn] = oldGrid[row][column];
-            oldGrid[row][column] = 0;
-            grid = oldGrid;
-            hasMoved = true;
-        }
-    }
-    else if (moveRow == -1 || moveColumn == -1){
-        hasMoved = true;
-    }
-    return hasMoved;
-
-}
-
-bool checkForPiece(int pieceRow, int pieceColumn, string player){
-    bool found = false;
-    if (pieceRow > -1 && pieceColumn > -1 && pieceRow <= length && pieceColumn <= width){
-        if (player == "white's"){
-            if (grid[pieceRow][pieceColumn] == -1){
-                found = true;
+            if (player == "black's"){
+                if (grid[pieceRow][pieceColumn] == 1){
+                    found = true;
+                }
             }
+        }else if (pieceRow != -1 && pieceColumn != -1){
+            cout << "Your input was outside the range of the board, please try again" << endl;
         }
-        if (player == "black's"){
-            if (grid[pieceRow][pieceColumn] == 1){
-                found = true;
-            }
-        }
-    }else if (pieceRow != -1 && pieceColumn != -1){
-        cout << "Your input was outside the range of the board, please try again" << endl;
+        return found;
     }
-    return found;
-}
 
-int getBlackScore(){
-    return blackScore;
-}
+    //Getters
+    // Requires: nothing
+    // Modifies: nothing
+    // Effects: returns the score for the black team to determine a winner
+    int getBlackScore(){
+        return blackScore;
+    }
 
-int getWhiteScore(){
-    return whiteScore;
-}
+    // Requires: nothing
+    // Modifies: nothing
+    // Effects: returns the score for the white team to determine a winner
+    int getWhiteScore(){
+        return whiteScore;
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 
 using namespace std;
 
+/******************** Global function prototypes ***********************/
+
+// Requires: nothing
+// Modifies: cout
+// Effects: Commences the game of checkers, testing out the Board class
 void playCheckers();
+
+// Requires: nothing
+// Modifies: cin, cout
+// Effects: Gets and returns integer input from user
 int getInput();
 
 int main() {
@@ -11,7 +20,10 @@ int main() {
     return 0;
 }
 
+/***************** Global function definitions *****************/
+
 void playCheckers(){
+    //Initialize variables for the start of the game, print instructions
     bool gameOver = false;
     bool hasMoved = false;
     int row, column, moveRow, moveColumn;
@@ -20,9 +32,25 @@ void playCheckers(){
     cout << "Ready for some checkers? Enter 0 for either of the piece selection inputs to quit the game" << endl;
     cout << "Also, if you select a piece but want to move a different one instead, enter 0 for either of the movement inputs" << endl;
 
+    //Main game loop, repeats every turn
     while(gameOver == false){
+        //Print the board, display a special message once a score reaches 12
         b1.printBoard();
+        if (b1.getWhiteScore() == 12){
+            cout << "*********************" << endl;
+            cout << "WHITE TEAM HAS WON!!!" << endl;
+            cout << "*********************" << endl;
+            gameOver = true;
+            break;
+        }else if (b1.getBlackScore() == 12){
+            cout << "*********************" << endl;
+            cout << "BLACK TEAM HAS WON!!!" << endl;
+            cout << "*********************" << endl;
+            gameOver = true;
+            break;
+        }
 
+        //Get input for the piece selection position, quit game if user inputs 0
         cout << "It is " + player + " turn."<< endl;
         
         cout << "\nPlease enter the row # of the piece you would like to select to move: ";
@@ -36,7 +64,9 @@ void playCheckers(){
             break;
         }
 
+        //Verify piece is at given position, if so enter the game movement loop
         if (b1.checkForPiece(row, column, player)){
+            //Get input for the movement position, loop if player fails to supply a valid movement position
             while(!hasMoved){
                 cout << "\nPlease enter the row # of the tile you would like to move to: ";
                 moveRow = getInput() - 1;
@@ -44,7 +74,7 @@ void playCheckers(){
                 cout << "\nPlease enter the column # of the tile you would like to move to: ";
                 moveColumn = getInput() - 1;
 
-            
+                //Attempt to move the piece using the given position, change player turn and exit loop if successful, display message otherwise
                 hasMoved = b1.movePiece(row, column, moveRow, moveColumn, player);
                 if (hasMoved && moveRow != -1 && moveColumn != -1){
                     if (player == "black's"){
@@ -66,6 +96,7 @@ void playCheckers(){
 
 int getInput(){
     int input;
+    //Get input, remove junk characters
     while (!(cin >> input))
         {
             cin.clear();
